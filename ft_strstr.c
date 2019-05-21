d6a6a83e3c581276af38d78dffaf01872da3b1a7
TshireletsoMentor <tmentor@student.wethinkcode.co.za>
2019-05-21 21:28:03 +0200

    created ft_strstr.c

---
+++ ft_strstr.c
@@ -0,0 +1,23 @@
+
+
+char		ft_strstr(*s1, *s2)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	if (s1[i] == '\0')
+	{
+		return ((char *)s1);
+	}
+	while (s1[i] != '\0')
+	{
+		if (s1[i] = s2[j])
+		{
+			
+		}
+		i++;
+	}
+
+}
