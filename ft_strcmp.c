fb254872cd1cee0a3732321f96e8e994692c50a0
Tshireletso MENTOR <tmentor@c4r17s7.wethinkcode.co.za>
2019-05-21 13:40:42 +0200

    ft_strcmp.c returns unsigned char

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: tmentor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 13:04:45 by tmentor           #+#    #+#             */
-/*   Updated: 2019/05/20 15:10:29 by tmentor          ###   ########.fr       */
+/*   Updated: 2019/05/21 13:27:35 by tmentor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ int		ft_strcmp(char *s1, char *s2)
 	{
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
