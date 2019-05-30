a02156badd71ef2dd0f380065c2f469e6764f16a
Tshireletso MENTOR <tmentor@c4r17s7.wethinkcode.co.za>
2019-05-30 11:02:32 +0200

    updated

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,16 +6,16 @@
 /*   By: tmentor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 10:42:47 by tmentor           #+#    #+#             */
-/*   Updated: 2019/05/30 10:48:43 by tmentor          ###   ########.fr       */
+/*   Updated: 2019/05/30 10:57:52 by tmentor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Libft.h"
 
-void	ft_putstr_fd(const char *str, int fd)
+void		ft_putstr_fd(const char *str, int fd)
 {
 	int i;
-	
+
 	i = 0;
 	if (str)
 	{
