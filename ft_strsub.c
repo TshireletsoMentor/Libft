f084a8c1f160d3df99123345b5f85e633d974c56
Tshireletso MENTOR <tmentor@c4r18s4.wethinkcode.co.za>
2019-06-09 09:41:50 +0200

    updated

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: tmentor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 15:07:01 by tmentor           #+#    #+#             */
-/*   Updated: 2019/06/08 15:30:36 by tmentor          ###   ########.fr       */
+/*   Updated: 2019/06/09 09:40:43 by tmentor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ char		*ft_strsub(char const *s, unsigned int start, size_t len)
 	size_t	n;
 
 	n = 0;
-	if (s == NULL || start > strlen(s) ||
-			(new = (char *)malloc(sizeof(char) * (len + 1))))
+	if (s == NULL || start > ft_strlen(s) ||
+			(new = (char *)malloc(sizeof(char) * (len + 1))) == NULL)
 		return (NULL);
 	while (n++ < len && s[start] != '\0')
 		new[n - 1] = s[start + n - 1];
